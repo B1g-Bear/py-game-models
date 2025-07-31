d0dc841ad367cc982193753ee90050b254200bc6
Andrii Novikov <forytube2023@gmail.com>
2025-07-31 14:38:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from pathlib import Path
 from db.management.commands.import_players import import_players
 
+
 def main() -> None:
     pass
+
 
 def run() -> None:
     file_path = Path("db/players.json")
     import_players(file_path)
+
 
 if __name__ == "__main__":
     run()
