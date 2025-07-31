3d955460f46f636e1a891edf56be59fb2078282c
Andrii Novikov <forytube2023@gmail.com>
2025-07-31 14:35:07 +0300

    Update

--- db/management/commands/import_players.py
+++ db/management/commands/import_players.py
@@ -1,37 +1,26 @@
-import json
 from pathlib import Path
+import json
+from db.models import Player, Race, Skill, Guild
 
-from django.core.management.base import BaseCommand
-from db.models import Race, Skill, Guild, Player
-
-
-def import_players() -> None:
-    file_path = Path("players.json")
-
-    with file_path.open(encoding="utf-8") as file:
-        players_data = json.load(file)
+def import_players(file_path: Path) -> None:
+    with file_path.open(encoding="utf-8") as f:
+        players_data = json.load(f)
 
     for player_data in players_data:
         race, _ = Race.objects.get_or_create(name=player_data["race"])
+        guild_name = player_data.get("guild")
         guild = None
-
-        if guild_name := player_data.get("guild"):
+        if guild_name:
             guild, _ = Guild.objects.get_or_create(name=guild_name)
 
-        player, _ = Player.objects.get_or_create(
+        player = Player.objects.create(
             nickname=player_data["nickname"],
-            defaults={
-                "race": race,
-                "guild": guild,
-                "level": player_data["level"],
-            },
+            email=player_data["email"],
+            bio=player_data.get("bio", ""),
+            race=race,
+            guild=guild,
         )
 
-        for skill_name in player_data["skills"]:
+        for skill_name in player_data.get("skills", []):
             skill, _ = Skill.objects.get_or_create(name=skill_name)
             player.skills.add(skill)
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options) -> None:
-        import_players()
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from pathlib import Path
 from db.management.commands.import_players import import_players
 
+def main() -> None:
+    pass
 
 def run() -> None:
-    import_players()
-
+    file_path = Path("db/players.json")
+    import_players(file_path)
 
 if __name__ == "__main__":
     run()
